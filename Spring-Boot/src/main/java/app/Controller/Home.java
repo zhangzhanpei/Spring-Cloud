825704495c8e4880a31f94fc5be7069d4f95866b
Zhangzhanpei <zhangzhanpei@gmail.com>
2018-08-31 17:26:09 +0800

    获取get参数

--- Spring-Boot/src/main/java/app/Controller/Home.java
+++ Spring-Boot/src/main/java/app/Controller/Home.java
@@ -2,26 +2,28 @@ package app.Controller;
 
 import app.Aop.Annotation.Timer;
 import app.Exception.MyException;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class Home {
-    @ResponseBody
     @GetMapping("/")
     public String index() throws Exception {
         // 抛出自定义异常
-        throw new MyException(404, "Spring Boot 同一错误处理");
+        throw new MyException(404, "Spring Boot 统一错误处理");
         // 抛出其他异常
         // throw new RuntimeException("RuntimeException");
     }
 
-    @ResponseBody
     @GetMapping("/aop")
     @Timer
     public String aop() {
         System.out.println("aop");
         return "aop";
+    }
+
+    @GetMapping("/params/{id:\\d+}")
+    public void params(@PathVariable int id, @RequestParam(value = "nickname", required = false) String queryString) {
+        System.out.println(id);
+        System.out.println(queryString);
     }
 }
